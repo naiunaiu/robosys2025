aefd9c97bbd331279bfa0f150b05f515fa4fb844
naiu <imagenaru2@gmail.com>
2025-10-30 12:45:34 +0900

    testを変更？

--- test.bash
+++ test.bash
@@ -1,12 +1,17 @@
-#!/bin/bash
+#!/bin/bash -xv
+# SPDX-FileCopyrightText: 2025 Satoh-Narumi
+# SPDX-License-Identifier: BSD-3-Clause
 
 ng () {
-	echo ${1}gyo-me is invalid
+	echo ${1}行目ダメよ
 	res=1
 }
 
 res=0
-[ "$a" = bana ] || ng "$LINENO"
-[ "$a" = banana ] || ng "$LINENO"
+
+out=$(seq 5 | ./plus)
+[ "${out}" = 15.0 ] || ng "$LINENO"
+
+[ "${res}" = 0 ] && echo OK
 
 exit $res
